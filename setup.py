cff93a7c4548edf2f9287ee1c194b5580cfb6713
bboc <bernhard.bockelbrink@gmail.com>
2015-11-13 23:18:27 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name = "todoist-converter",
+    packages = find_packages(),
+
+    entry_points = {
+        'console_scripts': [
+            'tdconv = tdconv:main',
+        ],
+    }
+
+)
