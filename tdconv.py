2987007f44deaa7efa7d1dd35e7a78b679b3091d
bboc <bernhard.bockelbrink@gmail.com>
2015-11-16 18:32:04 +0100

    added conversion from opml back to csv

--- tdconv.py
+++ tdconv.py
@@ -2,16 +2,38 @@
 from __future__ import print_function
 
 import argparse
+from collections import defaultdict
 import csv
 import json
 import os.path
 from string import Template
 import xml.etree.cElementTree as ET
 
-CONTENT = 'CONTENT'
-INDENT = 'INDENT'
-PRIORITY = 'PRIORITY'
+
+
+# Todoist csv fields
 TYPE = 'TYPE'
+CONTENT = 'CONTENT'
+PRIORITY = 'PRIORITY'
+INDENT = 'INDENT'
+AUTHOR = 'AUTHOR'
+RESPONSIBLE = 'RESPONSIBLE'
+DATE = 'DATE'
+DATE_LANG = 'DATE_LANG'
+
+
+# (ordered) list of fieldnames for Todoist CSV
+FIELDNAMES = TYPE,CONTENT,PRIORITY,INDENT,AUTHOR,RESPONSIBLE,DATE,DATE_LANG
+
+TYPE_TASK = 'task'
+TYPE_NOTE = 'note'
+
+FORMAT_MD = 'md'
+FORMAT_OPML = 'opml'
+FORMAT_CSV = 'todoist'
+
+
+NOTE_ATTRIB = '_note'
 
 
 def convert(args):
@@ -20,10 +42,12 @@ def convert(args):
     Rows: TYPE (task, note),CONTENT,PRIORITY(1-4),INDENT,AUTHOR,RESPONSIBLE,DATE,DATE_LANG
     """
 
-    if args.format == 'opml':
-        convert_to_opml(args)
+    if args.format.lower() == FORMAT_OPML:
+        convert_csv_to_opml(args)
+    elif args.format.lower() == FORMAT_CSV:
+        convert_opml_to_csv(args)
     else: 
-        convert_to_md(args)
+        convert_csv_to_md(args)
 
 
 def title(filename):
@@ -40,7 +64,8 @@ def target_name(filename, ext):
     return '.'.join((os.path.splitext(os.path.basename(filename))[0], ext))
 
 
-def convert_to_md(args):
+def convert_csv_to_md(args):
+    """Convert CSV to Markdown."""
     img = Template('![$name]($url)')
 
     with open(target_name(args.file, 'md'), 'w+') as target:
@@ -48,9 +73,9 @@ def convert_to_md(args):
         with open(args.file) as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                if row[TYPE] == 'task':
+                if row[TYPE] == TYPE_TASK:
                     print('#' * (int(row[INDENT])+1), row[CONTENT], '\n', file=target)
-                elif row[TYPE] == 'note':
+                elif row[TYPE] == TYPE_NOTE:
                     if row[CONTENT].strip().startswith(u'[[file'):
                         j = json.loads(row[CONTENT][8:-2])
                         print(img.substitute(name=j['file_name'], url=j['file_url']), '\n', file=target)
@@ -59,10 +84,10 @@ def convert_to_md(args):
         print('\n', file=target)
 
 
-def convert_to_opml(args):
+def convert_csv_to_opml(args):
+    """Convert Todoist CSV to OPML."""
 
     img = Template('Image "$name": $url')
-    NOTE = '_note'
 
     opml = ET.Element("opml", version='1.0')
     head = ET.SubElement(opml, 'head')
@@ -76,24 +101,64 @@ def convert_to_opml(args):
     with open(args.file) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            if row[TYPE] == 'task':
+            if row[TYPE] == TYPE_TASK:
                 level = int(row[INDENT])
                 current = ET.SubElement(parents[level-1], 'outline', text=row[CONTENT])
                 parents[level] = current
-            elif row[TYPE] == 'note':
+            elif row[TYPE] == TYPE_NOTE:
                 if row[CONTENT].strip().startswith(u'[[file'):
                     j = json.loads(row[CONTENT][8:-2])
                     new_note = img.substitute(name=j['file_name'], url=j['file_url'])
                 else:
                     new_note = row[CONTENT]
-                note = current.get(NOTE)
+                note = current.get(NOTE_ATTRIB)
                 if note: 
-                    current.set(NOTE, '\n\n'.join((note, new_note)))
+                    current.set(NOTE_ATTRIB, '\n\n'.join((note, new_note)))
                 else:
-                    current.set(NOTE, new_note)
+                    current.set(NOTE_ATTRIB, new_note)
 
     tree = ET.ElementTree(opml)
     tree.write(target_name(args.file, 'opml'), encoding='UTF-8', xml_declaration=True)
+
+
+def convert_opml_to_csv(args):
+    """Convert OPML file to Todoist CSV."""
+
+    tree = ET.parse(args.file)
+    opml = tree.getroot()
+    body = opml.find('body')
+    with open(target_name(args.file, 'csv'), 'w+') as target:
+        writer = csv.DictWriter(target, FIELDNAMES, restval='', extrasaction='raise')
+        # TODO: research and specify dialect
+
+        def make_row(type='', content='', indent = ''):
+            return {
+                TYPE: type,
+                CONTENT: content,
+                PRIORITY: '',
+                INDENT: indent,
+                AUTHOR: '',
+                RESPONSIBLE: '',
+                DATE: '',
+                DATE_LANG: '',
+            }
+
+        def process_element(outline, level=1):
+            # content
+            row = make_row(TYPE_TASK, outline.get('text'), level)
+            writer.writerow(row)
+            # note
+            note = outline.get(NOTE_ATTRIB)
+            if note:
+                row = make_row(TYPE_NOTE, note)
+                writer.writerow(row)
+            # separator
+            writer.writerow(make_row())
+            for subelement in outline.findall('outline'):
+                process_element(subelement, level+1)
+
+        for outline in body:
+            process_element(outline)
 
 
 def main():
@@ -103,7 +168,7 @@ def main():
     parser.add_argument('--verbose', '-v', action='count', default=0,
                         help='increase level of verbosity (repeat up to 3 times)')
     parser.add_argument('--format', '-f', default='md',
-                        help='format of target file: md (later maybe also opml')
+                        help='format of target file: md, opml, todoist')
 
     parser.add_argument('file',
                         help='file to convert')
