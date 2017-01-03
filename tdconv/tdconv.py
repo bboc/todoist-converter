177a41d9cd240db2e3cb59390235e83e6f877938
bboc <bernhard.bockelbrink@gmail.com>
2017-01-03 17:58:11 +0100

    updated command description

--- tdconv/tdconv.py
+++ tdconv/tdconv.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import argparse
+from textwrap import dedent
 
 from markdown import CsvToMarkdownConverter
 from opml import OpmlToCsvConverter, CsvToOpmlConverter
@@ -33,12 +34,15 @@ def convert(args):
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Convert todoist template files to other formats.')
+        description=dedent("""Convert Todoist template files (and Todoist backups) to other formats.
+            To convert an entire backup use 
+            ls -bp | xargs -I xx tdconv -df taskpaper "xx"
+            """))
 
     parser.add_argument('--verbose', '-v', action='count', default=0,
                         help='increase level of verbosity (repeat up to 3 times)')
     parser.add_argument('--format', '-f', default='md',
-                        help='format of target file: md, opml, todoist')
+                        help='format of target file: md, opml, todoist, taskpaper')
     parser.add_argument('--download', '-d', action="store_true", default=False,
                         help='download attachments')
     parser.add_argument('file',
