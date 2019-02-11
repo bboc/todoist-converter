8ffe192436a1a509db0455f35ff9fdd17d21cf2f
bboc <bernhard.bockelbrink@gmail.com>
2019-02-11 18:33:34 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+install_requires = [
+    # nothing
+]
+
+tests_require = [
+    'nose',
+]
+
 setup(
     name="todoist-converter",
+    version="0.4",
+    author="Bernhard Bockelbrink",
+    author_email="bernhard.bockelbrink@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bboc/todoist-converter",
     packages=find_packages(),
-
+    install_requires=install_requires,
+    tests_require=tests_require,
+    test_suite="nose.collector",
     entry_points={
         'console_scripts': [
             'tdconv = tdconv.tdconv:main',
